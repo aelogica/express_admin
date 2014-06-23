7e66b2ca3d090b74d191fb2db3d6dc1147eb41b4
Karlo Soriano and Terence Ponce <dev+karlo+terence@aelogica.com>
2014-06-24 07:39:13 +0800

    [#73723576] Move js and css from app_express

--- app/assets/javascripts/express_admin/admin.js
+++ app/assets/javascripts/express_admin/admin.js
@@ -4,6 +4,46 @@
 //= require jquery_ujs
 //= require foundation
 //= require message-bus
+
 $(function() {
   $(document).foundation();
+
+  window.startAnimateProgress = function(message) {
+    var numberOfDots = 3;
+    var dots = numberOfDots - 1;
+    var dotSpan = "";
+    for(i = 0; i < dots; i++) {
+      dotSpan += "<span class='dot'>.</span>"
+    }
+    $('#nav-js-alert .alert-box').html(message + "." + dotSpan);
+
+    window.intervalId = setInterval( function() {
+
+      for(i = 0; i < dots; i++) {
+        $($('.dot')[i]).removeClass('dot')
+      }
+
+      dots++;
+      if (dots > numberOfDots) {
+        $('#nav-js-alert .alert-box span').addClass('dot')
+        dots = 1;
+      }
+
+    }, 500)
+    $('#nav-js-alert .alert-box').addClass('info');
+    $('#nav-js-alert').show();
+  };
+
+  window.stopAnimateProgress = function() {
+    clearInterval(window.intervalId);
+  };
+
+  $(".gallery a.addon").click(function(e) {
+  e.preventDefault();
+  });
+
+  $("a.addon-cancel").click(function(e) {
+    e.preventDefault();
+    $('a.close-reveal-modal').trigger('click');
+  });
 });
