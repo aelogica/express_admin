11873283467a602627abd251e39bbfbc65252df8
Karlo Soriano and Terence Ponce <dev+karlo+terence@aelogica.com>
2014-06-24 09:27:17 +0800

    [#73763550] Extract flash_class helper from app_express

--- app/helpers/express_admin/application_helper.rb
+++ app/helpers/express_admin/application_helper.rb
@@ -1,4 +1,12 @@
 module ExpressAdmin
   module ApplicationHelper
+    def flash_class(key)
+      case key
+      when 'notice' then 'info'
+      when 'success' then 'success'
+      when 'alert' then 'warning'
+      when 'error' then 'alert'
+      end
+    end
   end
 end
