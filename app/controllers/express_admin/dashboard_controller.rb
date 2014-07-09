558363f1c9890aad5932e4125431fe0d6ac80fbc
Ace Subido <dev+ace@aelogica.com>
2014-07-09 11:17:40 +0800

    [#74488672] Major style changes for main containers, main container is now provided by engines, not coming from express_admin anymore

--- app/controllers/express_admin/dashboard_controller.rb
+++ app/controllers/express_admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ require_dependency "express_admin/application_controller"
 
 module ExpressAdmin
   class DashboardController < AdminController
+    layout 'layouts/express_admin/application'
     def show
     end
   end
