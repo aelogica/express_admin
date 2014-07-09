b20cb5e35c192a8dc5f2f0a2d7cdf99ed2e98f51
Ace Subido <dev+ace@aelogica.com>
2014-07-09 14:56:29 +0800

    [#74488672] Cleanup cardlist files and markup

--- app/controllers/express_admin/dashboard_controller.rb
+++ app/controllers/express_admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ require_dependency "express_admin/application_controller"
 
 module ExpressAdmin
   class DashboardController < AdminController
-    layout 'layouts/express_admin/application'
+    layout 'express_admin/application'
     def show
     end
   end
