0537f438c81e06bbe26a4b2691ec350c76352f8f
Ace Subido and Terence Ponce <dev+ace+terence@aelogica.com>
2014-07-10 08:58:54 +0800

    [#74488672] Add fonts to asset precompile
    [#74488672] Add fix to font asset precompile paths

--- lib/express_admin/engine.rb
+++ lib/express_admin/engine.rb
@@ -24,10 +24,15 @@ module ExpressAdmin
       engine_assets_path = File.join(File.dirname(__FILE__), '..', '..', 'app', 'assets')
       all_assets = Dir.glob File.join(engine_assets_path, 'stylesheets', '**', '*.css*')
       all_assets += Dir.glob File.join(engine_assets_path, 'javascripts', '**', '*.js*')
+      all_assets += Dir.glob File.join(engine_assets_path, 'fonts', '**', '*.*')
       all_assets.each {|path| path.gsub!("#{engine_assets_path}/stylesheets/", '')}
       all_assets.each {|path| path.gsub!("#{engine_assets_path}/javascripts/", '')}
+      all_assets.each {|path| path.gsub!("#{engine_assets_path}/fonts/", '')}
       all_assets.each {|path| path.gsub!(/.(scss|coffee)$/, '')}
+
+      Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
       Rails.application.config.assets.precompile += all_assets
+
       if Kernel.const_defined?('AppExpress::Engine')
         Rails.application.config.assets.precompile += %w( message-bus.js )
       end
