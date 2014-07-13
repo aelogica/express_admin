0e45e8211af1ce2ac3e0f8adcb9636b8298de4e9
Steven Talcott Smith <steve@aelogica.com>
2014-07-12 22:09:20 -0700

    [#74908472] remove oauth code that doesnot belong here

--- app/controllers/express_admin/application_controller.rb
+++ app/controllers/express_admin/application_controller.rb
@@ -1,8 +1,4 @@
 module ExpressAdmin
   class ApplicationController < ActionController::Base
-    def redirect_to_oauth
-      redirect_to Rails.application.routes.url_helpers.user_omniauth_authorize_path(:appexpress) unless user_signed_in?
-    end
-    helper_method :redirect_to_oauth
   end
 end
