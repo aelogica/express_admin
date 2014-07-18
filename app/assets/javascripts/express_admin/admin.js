926bb8619613b013bfd63ae4ae46ad248f1298b6
Ace Subido <dev+ace@aelogica.com>
2014-07-18 16:14:31 +0800

    [#75238848] Add colorpicker, datepicker and timepicker to express admin

--- app/assets/javascripts/express_admin/admin.js
+++ app/assets/javascripts/express_admin/admin.js
@@ -2,6 +2,7 @@
 // All this logic will automatically be available in application.js.
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.datepicker
 //= require foundation
 //= require select2
 //= require underscore
