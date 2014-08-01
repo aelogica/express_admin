b1b543ab77ef217ce9a50de48560feabf771545d
Terence Ponce <terenceponce@gmail.com>
2014-08-01 10:15:00 +0800

    [#76085552] Require authentication for /admin

--- app/controllers/express_admin/admin_controller.rb
+++ app/controllers/express_admin/admin_controller.rb
@@ -2,6 +2,8 @@ require_dependency "express_admin/application_controller"
 
 module ExpressAdmin
   class AdminController < ApplicationController
+    before_filter :authenticate_user!
+
     layout "express_admin/admin"
   end
 end
