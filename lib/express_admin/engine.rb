30fe4e3b065fbba943dae1130725cbd5c01f6147
Terence Ponce <terence@aelogica.com>
2014-08-13 15:24:18 +0800

    Remove helper import

--- lib/express_admin/engine.rb
+++ lib/express_admin/engine.rb
@@ -45,12 +45,6 @@ module ExpressAdmin
       end
     end
 
-    initializer 'express_admin.action_controller' do |app|
-      ActiveSupport.on_load :action_controller do
-        helper ExpressAdmin::AdminHelper
-      end
-    end
-
     config.autoload_paths += Dir[ExpressAdmin::Engine.root.join('app', 'jobs')]
 
     config.admin_mount_point = '/admin'
