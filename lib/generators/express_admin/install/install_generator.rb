88387b80f56cb97978a4bb7566594b4fe3f84345
Terence Ponce <terence@aelogica.com>
2014-08-21 10:01:26 +0800

    Insert rails_admin inside the app_express gem group on installation

--- lib/generators/express_admin/install/install_generator.rb
+++ lib/generators/express_admin/install/install_generator.rb
@@ -10,7 +10,11 @@ class ExpressAdmin::InstallGenerator < Rails::Generators::Base
     git add: "config/routes.rb"
     git commit: "'-m [express_admin] install express_admin'"
 
-    append_file 'Gemfile', "\ngem 'rails_admin', github: 'aelogica/express_rails_admin'"
+    rails_admin_gem_line = <<-EXPRESS_RAILS_ADMIN
+  gem 'rails_admin', github: 'aelogica/express_rails_admin'
+EXPRESS_RAILS_ADMIN
+    inject_into_file "Gemfile", rails_admin_gem_line, after: "group :app_express do\n"
+
     Bundler.clean_system("bundle install")
     Bundler.clean_system("rails generate rails_admin:install admin/manage")
     git add: "."
