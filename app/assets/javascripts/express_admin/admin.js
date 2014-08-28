16678753c9042fefd73531d6106674caa712000a
Karlo Nicholas T. Soriano and Ricky Hurtado <dev+karlo+ricky@aelogica.com>
2014-08-28 09:07:05 +0800

    [#77587024] Enhance the flash message for modules

--- app/assets/javascripts/express_admin/admin.js
+++ app/assets/javascripts/express_admin/admin.js
@@ -56,9 +56,6 @@ $(function() {
     e.preventDefault();
     $('a.close-reveal-modal').trigger('click');
   });
-
-  // Fade flash messages
-  $('.alert-box:not(.sticky)').fadeOut(2000)
 });
 
 String.prototype.repeat = function( num )
