29a2516b7d1a6e69adedf29d7a8b7d7b620a7e2e
Steven Talcott Smith <steve@aelogica.com>
2014-10-02 18:02:00 -0700

    Comment out code I want to go away

--- app/helpers/express_admin/admin_helper.rb
+++ app/helpers/express_admin/admin_helper.rb
@@ -37,37 +37,37 @@ module ExpressAdmin
         map {|engine| engine.express_admin_menu}
     end
 
-    def admin_navbar_items
-      admin_navbar = RecursiveOpenStruct.new()
-      admin_navbar.title_partial = render title_partial
-      admin_navbar.profile_partial = render 'shared/express_admin/navigation_bar_profile'
-      admin_navbar.menus = admin_menus.map do |menu|
-        if menu.submenus
-          menu.main.path = '#' # for dropdown
-          menu.submenus.each do |submenu|
-            submenu.path = eval(submenu.path)
-          end
-        else
-          menu.main.path = eval(menu.main.path)
-        end
+    # def admin_navbar_items
+    #   admin_navbar = RecursiveOpenStruct.new()
+    #   admin_navbar.title_partial = render title_partial
+    #   admin_navbar.profile_partial = render 'shared/express_admin/navigation_bar_profile'
+    #   admin_navbar.menus = admin_menus.map do |menu|
+    #     if menu.submenus
+    #       menu.main.path = '#' # for dropdown
+    #       menu.submenus.each do |submenu|
+    #         submenu.path = eval(submenu.path)
+    #       end
+    #     else
+    #       menu.main.path = eval(menu.main.path)
+    #     end
 
-        menu
-      end
+    #     menu
+    #   end
 
-      admin_navbar.to_json
-    end
+    #   admin_navbar.to_json
+    # end
 
-    def admin_navbar
-      if current_user
-        (stylesheet_link_tag 'express_admin/sections/_header') +
-        (stylesheet_link_tag 'express_admin/shared/_navigation') +
-        content_tag(:div, id: 'appexpress-admin-navbar') do
-          content_tag(:script) do
-            "window.appexpress_admin_navbar = #{admin_navbar_items}".html_safe
-          end
-        end
-      end
-    end
+    # def admin_navbar
+    #   if current_user
+    #     (stylesheet_link_tag 'express_admin/sections/_header') +
+    #     (stylesheet_link_tag 'express_admin/shared/_navigation') +
+    #     content_tag(:div, id: 'appexpress-admin-navbar') do
+    #       content_tag(:script) do
+    #         "window.appexpress_admin_navbar = #{admin_navbar_items}".html_safe
+    #       end
+    #     end
+    #   end
+    # end
 
     def menu_item(name, path)
       content_tag(:li, link_to(name, path), class: is_active?(path))
