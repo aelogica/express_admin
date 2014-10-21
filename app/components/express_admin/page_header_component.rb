eb5de725ff3c9bd7f00e7b0589782047ab8413fe
Steven Talcott Smith <steve@aelogica.com>
2014-10-21 12:07:18 +0800

    Use new conditionality feature for page_header_component

--- app/components/express_admin/page_header_component.rb
+++ app/components/express_admin/page_header_component.rb
@@ -1,47 +1,39 @@
 module ExpressAdmin
   class PageHeaderComponent < ExpressTemplates::Components::Base
-      # if content_for?(:page_header)
-      #   .row.page-header
-      #     %h1
-      #       = yield :page_header
-      #     - if content_for?(:page_header_lead)
-      #       %p.lead
-      #         = yield :page_header_lead
+    ETC = ExpressTemplates::Components
 
-    emits header_wrap: -> {
-                h1 {
-                  _yield
-                }
+    class PageHeader < ExpressTemplates::Components::Base
+      include ETC::Capabilities::Conditionality
 
-            },
-          lead_wrap: -> {
-              p.lead {
-                _yield
+      emits {
+              h1 {
+                content_for(:page_header)
               }
-            },
-          header: -> {
-            content_for(:page_header)
-          },
-          lead: -> {
-            content_for(:page_header_lead)
-          }
+            }
 
+      only_if -> { content_for?(:page_header) }
 
-    using_logic { |c|
-      _o = ''
-      if content_for?(:page_header)
-        _o << c._wrap_using(:header_wrap, self) {
-          eval(c[:header])
-        }
-      end
-      if content_for?(:page_header_lead)
-        _o << c._wrap_using(:lead_wrap, self) {
-          eval(c[:lead])
-        }
-      end
-      _o.html_safe
+    end
+
+    class PageHeaderLead < ExpressTemplates::Components::Base
+      include ETC::Capabilities::Conditionality
+
+      emits {
+              p.lead {
+                content_for(:page_header_lead)
+              }
+            }
+
+      only_if -> { content_for?(:page_header_lead) }
+
+    end
+
+    emits {
+        page_header
+        page_header_lead
     }
 
-
   end
+
+
 end
