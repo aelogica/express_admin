9802eb729dc9040d14aef9c59e4609ff3a2db52b
Ricky Hurtado and Terence Ponce <dev+ricky+terence@aelogica.com>
2014-11-03 10:40:00 +0800

    Add div wrapper for the flash message

--- app/components/express_admin/flash_message_component.rb
+++ app/components/express_admin/flash_message_component.rb
@@ -2,14 +2,15 @@ module ExpressAdmin
   class FlashMessageComponent < ExpressTemplates::Components::Base
 
     Helper = ExpressAdmin::FlashMessageComponent  # for convenience
-
     helper(:safe_message) {|message| message[1] }
     helper(:classes) {|message| "flash nav-alert alert-box #{flash_class(message[0])}" }
 
     emits {
-      div(class: Helper.classes('{{flash_message}}'), data: {alert: ''}) {
-        safe_message('{{flash_message}}')
-        a.close(:href => "#") { "&times;" }
+      div {
+        div(class: Helper.classes('{{flash_message}}'), data: {alert: ''}) {
+          safe_message('{{flash_message}}')
+          a.close(:href => "#") { "&times;" }
+        }
       }
     }
 
