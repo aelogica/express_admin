291fc523ccd060f9dde6f4652d7041bb02810115
Ricky Hurtado and Terence Ponce <dev+ricky+terence@aelogica.com>
2014-11-06 15:08:17 +0800

    [#82011998 #82012014] Rename type into taxonomy

--- db/migrate/20141104222737_create_express_admin_taxonomies.rb
+++ db/migrate/20141104222737_create_express_admin_taxonomies.rb
@@ -3,7 +3,7 @@ class CreateExpressAdminTaxonomies < ActiveRecord::Migration
     create_table :express_admin_taxonomies do |t|
       t.integer    :term_id
       t.integer    :parent_term_id
-      t.string     :type
+      t.string     :taxonomy
       t.text       :description
       t.timestamps
     end
