8b1fd796b152dd0682abeb40ee7fe0820e2dcad2
Eumir Gaspar and Neil Gardose <coron+eumir+neilkim@aelogica.com>
2015-02-27 09:05:21 +0800

    [#87178286] Generate controller

--- lib/generators/express_admin/scaffold/scaffold_generator.rb
+++ lib/generators/express_admin/scaffold/scaffold_generator.rb
@@ -4,11 +4,29 @@ module ExpressAdmin
   module Generators
     class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
       source_root File.expand_path("../templates", __FILE__)
+      attr_reader :project_name, :project_path, :view_path, :resource_class
+
+      def create_root_folder
+        @project_name = if Rails.application
+                          Rails.application.class.parent_name
+                        else
+                          class_path.first.classify
+                        end
+        @project_path = @project_name.underscore
+        @resource_class = "#{@project_name}::#{singular_name.camelize}".classify
+        @view_path = File.join('app/views', @project_path, 'admin', plural_name)
+        empty_directory File.join("app/views", @project_path, "admin", controller_file_path)
+      end
+
       def copy_view_files
         available_views.each do |view|
           filename = filename_with_extensions(view)
-          template "#{view}.html.et.erb", File.join("app/views/", Rails.application.class.parent_name.underscore, "admin", controller_file_path, filename)
+          template "#{view}.html.et.erb", File.join("app/views", @project_path, "admin", controller_file_path, filename)
         end
+      end
+
+      def create_controller_file
+        template "controller/controller.rb", File.join("app/controllers", @project_path, "admin", "#{controller_file_path}_controller.rb")
       end
 
       hook_for :form_builder, :as => :scaffold
@@ -16,6 +34,10 @@ module ExpressAdmin
       protected
         def available_views
           %w(index)
+        end
+
+        def available_actions
+          %w(index show update destroy)
         end
 
         def handler
