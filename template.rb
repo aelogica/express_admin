c6fb74f452bd5882a1f635fe094c868b7848fce9
Karlo Nicholas Soriano and Nestor Pestelos <dev+karlo+nestor@aelogica.com>
2015-03-18 09:47:02 +0800

    update template.rb

--- template.rb
+++ template.rb
@@ -1,49 +1,8 @@
-require 'bundler'
-Bundler.setup
-
-git :init
-git add: "."
-git commit: "-a -m 'Initial commit'"
-
-ruby_version = ask("What ruby version do you want to use ?")
-ruby_gemset = ask("What gemset do you want to use ?")
-
-file ".ruby-version", ruby_version
-file ".ruby-gemset", ruby_gemset
-
-git add: ".ruby-version"
-git add: ".ruby-gemset"
-git commit: "-a -m 'Ruby version and gemset'"
-
-gem 'express_templates'
-gem 'modernizr-rails'
-gem 'therubyracer'
-gem 'foundation-rails'
+gem 'sqlite3'
 gem 'devise'
+gem 'unicorn'
+gem 'resque'
 
-gem_group :app_express do
-  gem 'express_admin', github: 'aelogica/express_admin'
+gem_group :production do
+  gem 'rails_12factor'
 end
-
-app_express_gem_group = <<-GEM_GROUP
-    Bundler.require(:app_express, Rails.env) if defined?(Bundler)
-GEM_GROUP
-inject_into_file 'config/application.rb', app_express_gem_group, after: "class Application < Rails::Application\n"
-
-Bundler.clean_system "bundle install"
-
-git add: "Gemfile"
-git add: "Gemfile.lock"
-git commit: "-m 'Added gems'"
-
-Bundler.clean_system "rails generate foundation:install --force"
-git add: "."
-git commit: "-a -m 'Installed Foundation'"
-
-Bundler.clean_system "rails generate devise:install && rails generate devise User --force && rake db:migrate"
-git add: "."
-git commit: "-a -m 'Installed Devise'"
-
-Bundler.clean_system "rails generate express_admin:install --force"
-git add: "."
-git commit: "-a -m 'Installed ExpressAdmin'"
