e1e9a887dbaf76b506146b355e4b052b5e85ae7b
Karlo Nicholas Soriano and Nestor Pestelos <dev+karlo+nestor@aelogica.com>
2015-03-18 08:03:48 +0800

    remove express_rails_admin from express_admin's install generator

--- lib/generators/express_admin/install/install_generator.rb
+++ lib/generators/express_admin/install/install_generator.rb
@@ -1,22 +1,9 @@
 class ExpressAdmin::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-  desc "mount express_admin engine; migrate database"
+  desc "mount express_admin engine"
   def install
-    route_code = %Q{
-  mount ExpressAdmin::Engine, at: ExpressAdmin::Engine.config.admin_mount_point\n}
-    inject_into_file "#{Rails.root}/config/routes.rb", route_code, after: "Rails.application.routes.draw do\n"
-
-    rails_admin_gem_line = <<-EXPRESS_RAILS_ADMIN
-  gem 'rails_admin', github: 'aelogica/express_rails_admin'
-EXPRESS_RAILS_ADMIN
-    inject_into_file "Gemfile", rails_admin_gem_line, after: "group :app_express do\n"
-
-
-    Bundler.clean_system("bundle install")
-    Bundler.clean_system("rails generate rails_admin:install admin/manage")
-    Bundler.clean_system("rake express_admin:install:migrations")
-    Bundler.clean_system("rake db:migrate")
+    route "mount ExpressAdmin::Engine, at: ExpressAdmin::Engine.config.admin_mount_point"
   end
 
 end
