f1afca0412339d710c6316b68033eabb96503b95
Eumir Gaspar <coron+eumir@aelogica.com>
2015-06-26 09:13:43 +0800

    [#97601326] Make sidebar region inherit from layoutcomponent

--- app/components/express_admin/sidebar_region.rb
+++ app/components/express_admin/sidebar_region.rb
@@ -1,9 +1,13 @@
 module ExpressAdmin
-  class SidebarRegion < ExpressTemplates::Components::Container
+  class SidebarRegion < LayoutComponent
     emits -> {
-      div._sidebar_region {
+      div(dom_id, class: classes) {
         _yield
       }
     }
+
+    def classes
+      %w(sidebar-region).join @config.try(:[], :class)
+    end
   end
 end
