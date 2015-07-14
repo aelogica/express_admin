873b69d0c05d2b96c5513c1bd086b76c8ec50924
Steven Talcott Smith <steve@aelogica.com>
2015-07-14 10:54:45 +0800

    Add definition lists

---
+++ app/components/express_admin/definition_table.rb
@@ -0,0 +1,52 @@
+module ExpressAdmin
+  class DefinitionTable < ExpressTemplates::Components::Base
+    include ExpressTemplates::Components::Capabilities::Configurable
+    include ExpressTemplates::Components::Capabilities::Resourceful
+
+    emits -> {
+      table.definitions {
+        tbody {
+          definitions.each do |label, content|
+            tr {
+              th(align: "right") { label }
+              td { content }
+            }
+          end
+        }
+      }
+    }
+
+    def definitions
+      if @args.first.kind_of?(Array)
+        definitions_from_array(@args.first)
+      elsif @args.first.kind_of?(Hash)
+        definitions_from_hash(@args.first)
+      end
+    end
+
+    def definitions_from_hash(hash)
+      processed = hash.map do |k,v|
+        if v.kind_of? Symbol
+          [promptify(k), "{{resource.#{v}}}"]
+        else
+          [promptify(k), v]
+        end
+      end
+      Hash[processed]
+    end
+
+    def definitions_from_array(fields)
+      Hash[fields.map {|field| ["#{field.to_s.titleize}:", "{{resource.#{field}}}"]}]
+    end
+
+    private
+      def promptify(k)
+        if k.kind_of?(Symbol)
+          k.to_s.promptify
+        else
+          k.to_s
+        end
+      end
+
+  end
+end
--- lib/express_admin.rb
+++ lib/express_admin.rb
@@ -1,3 +1,4 @@
+require File.join(File.dirname(__FILE__), "core_extensions/string_promptify")
 require "express_admin/engine"
 
 module ExpressAdmin
