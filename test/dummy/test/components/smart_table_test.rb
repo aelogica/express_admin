0719c07997b98aeb8d7a06099ea0a32d5433dc21
Steven Talcott Smith <steve@aelogica.com>
2015-07-16 11:58:21 +0800

    [#98704634] fix failing tests for smart_table

--- test/dummy/test/components/smart_table_test.rb
+++ test/dummy/test/components/smart_table_test.rb
@@ -4,10 +4,25 @@ module Components
 
   class SmartTableTest < ActiveSupport::TestCase
 
-    fixtures :widgets
+    fixtures :widgets, :categories
 
+    def resource_assigns
+      {resource: Widget.new, collection: Widget.all}
+    end
+    def helpers
+      view = mock_action_view(resource_assigns)
+      class << view
+        def widget_path(widget_id)
+          "/widgets/#{widget_id.to_param}"
+        end
+        alias resource_path widget_path
+      end
+      view
+    end
     def compiled_widget_table(*args)
-      ExpressAdmin::SmartTable.new(:widgets, *args).compile
+      arbre {
+        smart_table(:widgets, *args)
+      }
     end
 
     test "should have hidden columns" do
@@ -15,12 +30,13 @@ module Components
       assert_no_match 'column7', compiled_widget_table
     end
 
-    test "iterates over collection setting local var correctly" do
-      assert_match '(@widgets.each_with_index.map do |widget, widget_index|', compiled_widget_table
+    test "iterates over collection setting table row id correctly" do
+      assert_match 'widget:298486374', compiled_widget_table
+      assert_match 'widget:980190962', compiled_widget_table
     end
 
-    test "renders cell contents within a substitution" do
-      assert_match /\{\{[^\{\}]*widget.column2[^\{\}]*\}\}/, compiled_widget_table
+    test "renders cell contents" do
+      assert_match '<td class="column2">Hammer</td>', compiled_widget_table
     end
 
     # this behavior could change after we implement support for decorators
@@ -31,7 +47,8 @@ module Components
     test "table cell shows related item name or display name" do
       # note this will move to a helper that will intelligently look
       # for decorated methods such as name or display_name
-      assert_match 'widget.category.try(:name) || widget.category.to_s', compiled_widget_table
+      assert_match '<td class="category_id">Toys</td>', compiled_widget_table
+      assert_match '<td class="category_id">Tools</td>', compiled_widget_table
     end
 
     test "table displays only columns specified if columns option provided" do
@@ -46,11 +63,11 @@ module Components
 
     test "table column titles may be customized" do
       compiled = compiled_widget_table(columns: {"Column3 is the Best" => :column3})
-      assert_match /<th class=\\"column3\\">Column3 is the Best/, compiled
+      assert_match /<th class="column3">Column3 is the Best/, compiled
     end
 
-    def widget_table_with_proc_column
-      return -> {
+    def compiled_widget_table_with_proc_column
+      arbre {
          smart_table(:widgets, columns: {
            "This column will error" => -> (widget) { doesnt_work },
            "This column will be fine" => -> (widget) { widget.column2.upcase },
@@ -58,51 +75,34 @@ module Components
       }
     end
 
-    def compiled_widget_table_with_proc_column
-      ExpressTemplates.compile(&widget_table_with_proc_column)
-    end
-
-
-    test "table cell values may be specified as procs" do
-      assert_match '-> (widget) { doesnt_work }', compiled_widget_table_with_proc_column
-    end
-
-    class DummyView
-      def initialize
-        @widgets = Widget.all
-      end
-      def widget_path(*args) ; "does not matter" ; end
-      def link_to(*args) ; widget_path(*args) ; end
-    end
-
     test "table cell has 'Error' if a value specified as a proc throws an exception" do
-      assert_match 'Error', DummyView.new.instance_eval(compiled_widget_table_with_proc_column)
+      assert_match 'Error', compiled_widget_table_with_proc_column
     end
 
     test "table cell contains result of proc.call if no exception is raised" do
-      assert_match 'LEGO', DummyView.new.instance_eval(compiled_widget_table_with_proc_column)
+      assert_match 'LEGO', compiled_widget_table_with_proc_column
     end
 
     test "table cell class is valid when proc is used" do
-      assert_match 'class=\"this_column_will_error\"', compiled_widget_table_with_proc_column
+      assert_match 'class="this_column_will_error"', compiled_widget_table_with_proc_column
     end
 
     test "attribute accessor appended with _link generates a link" do
-      fragment = -> {
+      fragment = arbre {
           smart_table(:widgets, columns: {
               'A link column' => :column3_link
             })
         }
-      assert_match 'link_to widget.column3, widget_path(widget.id)', ExpressTemplates.compile(&fragment)
+      assert_match /column3.*href="\/widgets\/(\d+)/, fragment
     end
 
     test "timestamp accessor appeneded with _in_words generates code that uses time_ago_in_words" do
-      fragment = -> {
+      fragment = arbre {
           smart_table(:widgets, columns: {
               'Created' => :created_at_in_words
             })
         }
-      assert_match "\(widget.created_at \? time_ago_in_words\(widget.created_at\)\+' ago' : 'never'\)", ExpressTemplates.compile(&fragment)
+      assert_match /class="created_at.*less than a minute ago/, fragment
     end
 
   end
