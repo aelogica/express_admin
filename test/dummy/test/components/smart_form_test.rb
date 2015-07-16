2684df53ff3e4df77bf6c7cf40945a1e818d9c5d
Steven Talcott Smith <steve@aelogica.com>
2015-07-16 13:04:03 +0800

    [#98704634] fix failing tests for smart_form

--- test/dummy/test/components/smart_form_test.rb
+++ test/dummy/test/components/smart_form_test.rb
@@ -3,92 +3,115 @@ require 'test_helper'
 module ExpressAdmin
 
   class SmartFormTest < ActiveSupport::TestCase
-    def compiled_widget_form(*args)
-      ExpressAdmin::SmartForm.new(:widget, *args).compile
+
+    def resource_assigns
+      {resource: Widget.new, collection: Widget.all}
+    end
+    def helpers
+      view = mock_action_view(resource_assigns)
+      class << view
+        def widget_path(widget_id)
+          "/widgets/#{widget_id.to_param}"
+        end
+        alias resource_path widget_path
+        def widgets_path
+          "/widgets"
+        end
+        alias collection_path widgets_path
+        def admin_widgets_path
+          "/admin/widgets"
+        end
+      end
+      view
+    end
+
+
+    def widget_form(*args)
+      arbre {
+        smart_form(:widgets, *args)
+      }
     end
 
     test "renders a form correct id" do
-      assert_match '<form id=\"widget\"', compiled_widget_form
+      assert_match '<form id="widget"', widget_form
     end
 
     test "uses inherited_resources path helpers to set correct action" do
-      action_attrib = 'action=\"{{@widget.try(:persisted?) ? widget_path(@widget) : widgets_path}}\"'
-      assert_match action_attrib, compiled_widget_form
+      action_attrib = 'action="/widgets"'
+      assert_match action_attrib, widget_form
     end
 
     test "action path can be overridden" do
-      custom_action = '{{something_custom}}'
-      action_attrib = 'action=\"'+custom_action+'\"'
-      assert_match action_attrib, compiled_widget_form(action: custom_action)
+      custom_action = '/something_custom'
+      assert_match /action="#{custom_action}"/, widget_form(action: custom_action)
     end
 
     test "category field is a select" do
-      assert_match 'select_tag("widget[category_id]"', compiled_widget_form
-      assert_match 'class: "select2"', compiled_widget_form
+      assert_match /<select.*name="widget\[category_id\]"/, widget_form
+      assert_match /<select.*name="widget\[category_id\]".*class="select2"/, widget_form
     end
 
     test "string field column2 is an input" do
-      assert_match 'text_field(:widget, :column2, {})', compiled_widget_form
+      assert_match /<input.*type="text".*name="widget\[column2\]"/, widget_form
     end
 
     test "text field column3 is a text_area" do
-      assert_match 'text_area(:widget, :column3, rows: 10)', compiled_widget_form
+      assert_match /<textarea.*name="widget\[column3\]".*rows="10"/, widget_form
     end
 
     test "datetime field column4 is a datetime_field" do
-      assert_match 'datetime_field(:widget, :column4, {})', compiled_widget_form
+      assert_match /<input.*type="datetime".*name="widget\[column4\]"/, widget_form
     end
 
-    test "boolean field column4 is a checkbox" do
-      assert_match 'check_box(:widget, :column5, {}, "1", "0")', compiled_widget_form
+    test "boolean field column5 is a checkbox" do
+      assert_match /<input.*type="checkbox".*name="widget\[column5\]"/, widget_form
     end
 
-    test "integer column is a number_field" do
-      assert_match 'number_field(:widget, :column7, {})', compiled_widget_form
+    test "integer column7 is a number_field" do
+      assert_match /<input.*type="number".*name="widget\[column7\]"/, widget_form
     end
 
     test "timestamps are not editable" do
-      assert_match 'Created At: {{@widget.try(:created_at)}}', compiled_widget_form
-      assert_match 'Updated At: {{@widget.try(:updated_at)}}', compiled_widget_form
+      assert_match /Created At:.*\n/, widget_form
+      assert_match /Updated At:.*\n/, widget_form
     end
 
     test "fields have labels" do
-      #%Q({{label_tag(\"widget_category_id\", \"Category\")}})
-      matches = compiled_widget_form.scan /label_tag\(\\"\w+\\", \"\w+\"\)/
+      matches = widget_form.scan /<label.*>/
       assert 7, matches.length
 
-      assert_match '%Q({{label_tag("widget_category_id", "Category")}})', compiled_widget_form
+      assert_match '<label for="widget_category_id">Category</label>', widget_form
     end
 
     test "fields are wrapped in a div" do
-      assert_match '<div class=\"field-wrapper\">', compiled_widget_form
+      assert_match '<div class="field-wrapper">', widget_form
     end
 
     test 'path prefix is provided' do
-      action_attrib = 'action=\"{{@widget.try(:persisted?) ? admin_widget_path(@widget) : admin_widgets_path}}\"'
-      assert_match action_attrib, compiled_widget_form(path_prefix: 'admin')
+      action_attrib = 'action="/admin/widgets"'
+      assert_match action_attrib, widget_form(path_prefix: 'admin')
     end
 
-    test "options_from_collection_for_select used for the related collection" do
-      assert_match 'options_from_collection_for_select(Category.all.select(:id, :name).order(:name)',
-                    compiled_widget_form
+    test "options for select come from the related collection" do
+      assert_match /<option.*>Toys<\/option>/, widget_form
+      assert_match /<option.*>Tools<\/option>/, widget_form
     end
 
-    test "options_from_collection_for_select use when a namespace is specified" do
-      assert_match 'options_from_collection_for_select(ExampleEngine::Category.all.select(:id, :name).order(:name)',
-                    compiled_widget_form(namespace: "example_engine")
+    test "options for select work when a namespace is specified" do
+      assert_match /<option.*>Toys<\/option>/, widget_form(namespace: "example_engine")
+      assert_match /<option.*>Tools<\/option>/, widget_form(namespace: "example_engine")
     end
 
-    test "collection_select with collection_ids is used for has_many_through associations" do
-      assert_match /collection_select.*tag_ids.*multiple: true/, compiled_widget_form
+    test "multiple select is used for has_many_through associations" do
+      assert_match /select multiple="multiple" name="widget\[tag_ids\]\[\]"/, widget_form
     end
 
     test "excluded fields are excluded" do
-      refute_match 'column3', compiled_widget_form(exclude: [:column3])
+      refute_match 'column3', widget_form(exclude: [:column3])
     end
 
     test "virtual attributes may be included" do
-      assert_match 'password', compiled_widget_form(virtual: [:password])
+      assert_match 'password', widget_form(virtual: [:password])
     end
   end
 end
