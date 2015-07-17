a23c4825c8c936d3062ab2a8b8d7e570b7da97d9
Steven Talcott Smith <steve@aelogica.com>
2015-07-17 14:20:28 +0800

    [#99281646] render sidebar partial if present; refactor addon_sidebar_component

--- app/components/express_admin/addon_sidebar_component.rb
+++ app/components/express_admin/addon_sidebar_component.rb
@@ -1,27 +1,32 @@
 module ExpressAdmin
+  # renders a sidebar partial if one is available
+  # otherwise uses menu.yml
   class AddonSidebarComponent < ExpressTemplates::Components::Base
-  # Replaces:
-  #
-  # .sidebar-wrapper
-  #   %aside.sidebar
-  #     %h5.title= <addon>::Engine.express_admin_menu.name
-  #     %ul.side-nav
-  #       - <addon>::Engine.express_admin_menu.items.each do |item|
-  #         %li
-  #           = link_to item.name, eval(item.path)
 
     emits -> {
-       section(class: 'module-sidebar') {
-         ul(class: 'menu-items') {
-           li(class: 'title') { current_menu_name }
-           helpers.current_menu.items.each do |item|
-             li {
-               link_to item.title, helpers.instance_eval(item.path)
-             }
-           end
-         }
-       }
-     }
+      begin
+        render("shared/#{helpers.current_module_path_name}/sidebar")
+      rescue Exception => e
+        section(class: 'module-sidebar') {
+          ul(class: 'menu-items') {
+            li(class: 'title') { current_menu_name }
+            li {
+              menu_list(helpers.current_menu.items)
+            }
+          }
+        }
+      end
+    }
+
+    def menu_list(list)
+      list.each do |item|
+        menu_list_item(item)
+      end
+    end
+
+    def menu_list_item(item)
+      link_to item.title.html_safe, helpers.instance_eval(item.path)
+    end
 
   end
 end
