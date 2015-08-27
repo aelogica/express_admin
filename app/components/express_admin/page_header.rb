c70f0c01e739242316088de7f2027d8455ffff33
Eumir Gaspar and Janice Concepcion <coron+eumir+janice@aelogica.com>
2015-08-27 11:05:25 +0800

    Fix page_header to not show enclosing elements when contents arent present

--- app/components/express_admin/page_header.rb
+++ app/components/express_admin/page_header.rb
@@ -1,14 +1,17 @@
 module ExpressAdmin
   class PageHeader < ExpressTemplates::Components::Base
-    ETC = ExpressTemplates::Components
 
     contains -> {
-      h1 {
-        content_for(:page_header) if content_for?(:page_header)
-      }
-      para(class: 'lead') {
-        content_for(:page_header_lead) if content_for?(:page_header_lead)
-      }
+      if content_for?(:page_header)
+        h1 {
+          content_for(:page_header)
+        }
+      end
+      if content_for?(:page_header_lead)
+        para(class: 'lead') {
+          content_for(:page_header_lead)
+        }
+      end
     }
   end
 end
