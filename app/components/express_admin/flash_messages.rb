80c0a332c0081a5a8766544c841c036af0772c9c
Earle Bunao and Kim Leabres <earle+earle+kim@aelogica.com>
2015-09-16 15:05:22 +0800

    Add style to flash message component
    - Enable close action for flash messages

--- app/components/express_admin/flash_messages.rb
+++ app/components/express_admin/flash_messages.rb
@@ -3,11 +3,24 @@ module ExpressAdmin
 
     contains -> {
       helpers.flash.each do |flash_message|
-        div(class:"flash nav-alert alert-box ", data: {alert: ''}) {
+        div(class:"flash nav-alert alert-box #{flash_message[0]}", data: {alert: ''}) {
           span flash_message[1]
           a(class: 'close', href: "#") { "&times;".html_safe }
         }
       end
+
+      content_for(:page_javascript) {
+        script {
+          %Q(
+            $(function() {
+              $('a.close').on('click', function(e){
+                e.preventDefault()
+                $(this).parents('.flash-messages').parent().remove()
+              })
+            });
+          ).html_safe
+        }
+      }
     }
 
   end
